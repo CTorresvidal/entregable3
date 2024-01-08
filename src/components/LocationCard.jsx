e8e1da4710b590c10c8eb226d7e4fd539aa0db58
Carlos <carlostorres2912@yahoo.com.com>
2024-01-08 11:48:19 -0500

    advances

---
+++ src/components/LocationCard.jsx
@@ -0,0 +1,16 @@
+import './styles/LocationCard.css'
+
+const LocationCard = ({ location }) => {
+  return (
+    <article>
+      <h2 className="location__title">{location?.name}</h2>
+      <ul className="location__resident">
+        <li className="loction__item"><span>Type:</span><span>{location?.type}</span></li>
+        <li className="loction__item"><span>Dimension:</span><span>{location?.dimension}</span></li>
+        <li className="loction__item"><span>Population:</span><span>{location?.residents.length}</span></li>
+      </ul>
+    </article>
+  )
+}
+
+export default LocationCard
